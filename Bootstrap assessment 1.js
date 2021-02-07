69384c8a033591367082f126c18f271b0aed154c
Jyotivirkar <78653352+Jyotivirkar@users.noreply.github.com>
2021-02-08 01:46:12 +0530

    Bootstrap assessment file created

---
+++ Bootstrap assessment 1.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" Dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>project</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+<div class ="navbar_header">
+<a class="navbar_brand" href="bootstrap_solution_signsignup.html">welcome</a>
+</div>
+<ul>
+  <li><a href="bootstrap_solution_signup.html"></a></li>
+</ul>
+</nav>
+<div class="container">
+<div class="jumbotron">
+<p>Various buttons are supported as part of these navbar forms, too. This is also a great reminder that vertical alignment utilities can be used to align different sized elements.</P>
+<P>Various buttons are supported as part of these navbar forms, too. This is also a great reminder that vertical alignment utilities can be used to align different sized elements.</P>
+</div>
+
+</div>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://images.app.goo.gl/jKgERRZWGVTrsZhr9" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://images.app.goo.gl/RJLpAC3SPhgrJr8e9" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://images.app.goo.gl/nELm4Km3vpvE9Mdq9" alt="...">
+    </div>
+  </div>
+
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+</body>
+</html>
